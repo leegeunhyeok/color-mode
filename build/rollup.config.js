301bbd4e6840f937596c4234209a2dd87c969c81
leegeunhyeok <lghlove0509@naver.com>
2019-09-30 11:09:59 +0900

    UPDATE: rollup config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs'
 import buble from 'rollup-plugin-buble'
-import { terser } from "rollup-plugin-terser"
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/entry.js',
@@ -12,7 +12,17 @@ export default {
     commonjs(),
     buble(),
     terser({
-      include: [/^.+\.min\.js$/, '*esm*']
+      include: [/^.+\.min\.js$/, '*esm*'],
+      output: {
+        comments(_node, comment) {
+          var text = comment.value;
+          var type = comment.type;
+          console.log(type, text)
+          if (type === 'comment1') {
+            return /@author|@license/i.test(text);
+          }
+        }
+      }
     })
   ]
 }
