850469a821b01c8e0a010e03925e0ca0ae6b05f8
leegeunhyeok <lghlove0509@naver.com>
2019-10-01 18:10:17 +0900

    UPDATE: Add Comments

--- src/index.js
+++ src/index.js
@@ -39,8 +39,8 @@ class ColorMode {
       throw new Error('Default theme is required')
     }
 
-    this._ROOT_ATTRIBUTE = 'theme'
-    this._DATA_PREFIX = 'color'
+    this._ROOT_ATTRIBUTE = 'theme' // <html {_ROOT_ATTRIBUTE}="themeName">
+    this._DATA_PREFIX = 'color' // data-{_DATA_PREFIX}
     this._TAGS = option.tags
     this._theme = option.initialTheme || 'default'
     this._themes = option.themes
@@ -63,11 +63,13 @@ class ColorMode {
   }
 
   _init () {
+    // Create <style type="text/css">...</style> tag
     const style = document.createElement('style')
     const styleAttribute = document.createAttribute('type')
     styleAttribute.value = 'text/css'
     style.attributes.setNamedItem(styleAttribute)
 
+    // Calculate animation duration
     const duration = this._duration / 1000
     if (duration > 0) {
       style.innerHTML += `[data-${this._DATA_PREFIX}] {
@@ -79,23 +81,35 @@ class ColorMode {
       }`.replace(/\s|\n/g, '')
     }
 
+    // Generate theme CSS
     Object.keys(this._themes).forEach(themeName => {
       style.innerHTML += this._generateThemeStyleSheet(themeName)
     })
 
+    // Add <style> tag to <head>
     document.head.appendChild(style)
+
+    // ColorMode global init status (for init once)
     window.__COLOR_MODE_INIT__ = true
   }
 
   _generateThemeStyleSheet (themeName) {
+    // css string
     let css = ''
+
+    // [parentSelector] [theme] { style }
     const parentSelector = `[${this._ROOT_ATTRIBUTE}="${themeName}"]`
     const targetTheme = this._themes[themeName]
 
+    /**
+     * Convert camelcase to CSS attribute type (hello-world)
+     * @param {string} word Camelcase string
+     */
     function toBarCase (word) {
       let val = ''
       const isUpperCase = word => word === word.toUpperCase()
 
+      // Split by uppercase
       word.split(/([A-Z])/g).forEach(el => {
         if (isUpperCase(el)) {
           val += '-' + el.toLowerCase()
@@ -107,38 +121,65 @@ class ColorMode {
       return val
     }
 
-    const detailStyleGeneratedStatus = {}
+    // Detail style must be initialize once
+    const detailStyleGeneratedStatus = {} // for status check
 
+    // All tags
     Object.keys(this._TAGS).forEach(tag => {
+      // targetTheme's color
       Object.keys(targetTheme).forEach(color => {
+        // tag's value
+        // string or object
+        /* Type ->
+          - string: Style attribute name
+          - object: Detail style defined object */
         const value = this._TAGS[tag]
 
+        // Style attribute name
         if (typeof value === 'string') {
           css += `${parentSelector} [data-${this._DATA_PREFIX}="${tag}:${color}"]{`
           css += `${value}:${targetTheme[color]};}`
         } else if (typeof value === 'object') {
+          // Detail style defined object
+
+          // If already initialized => skip
           if (detailStyleGeneratedStatus[tag]) {
             return
           }
 
+          // Set init status to true
           detailStyleGeneratedStatus[tag] = true
+
           css += `${parentSelector} [data-${this._DATA_PREFIX}="${tag}"]{`
+
+          // Key of style defined object
           Object.keys(value).forEach(k => {
-            let color = k
+            // value: style defined object
+            // k: key of style defined object
+            // value[k]: value
+
+            // value[k] will be color scheme string or theme's color name
+            let color = value[k]
+
+            // Start with @ => theme's color
             if (value[k].charAt(0) === '@') {
+              // Find theme's color
               color = targetTheme[value[k].slice(1)]
 
+              // value[k] color of theme is not found
               if (!color) {
                 throw new Error(
                   `Color name '${k.slice(1)}' not found in '${themeName}' theme`
                 )
               }
             }
+
+            // Add attribute with color
             css += `${toBarCase(k)}:${color};`
           })
           css += '}'
         } else {
-          // TODO: Throw type error
+          throw new Error(`tag value must be string or object. but given '${typeof value}'`)
         }
       })
     })
@@ -146,31 +187,46 @@ class ColorMode {
     return css
   }
 
+  /**
+   * Create attribute and set value
+   * @param {string} value Attribute value
+   * @returns {Attr}
+   */
   _createThemeAttribute (value) {
     const attr = document.createAttribute(this._ROOT_ATTRIBUTE)
     attr.value = value
     return attr
   }
 
+  /**
+   * Return current theme name
+   * @returns {string}
+   */
   get currentTheme () {
     return this._theme
   }
 
+  /**
+   * Return all of theme names
+   * @returns {array}
+   */
   getThemeList () {
     return Object.keys(this._themes)
   }
 
-  getColorPalette (themeName) {
-    return this._themes[themeName]
-  }
-
+  /**
+   * Change color theme to {themeName}
+   * @param {string} themeName Theme name
+   */
   set (themeName) {
+    // If theme of themeName not found, set to fallbackTheme
     if (!this._themes[themeName]) {
       console.error(`'${themeName}' theme is not exist`)
       themeName = this._fallbackTheme
     }
     this._theme = themeName
 
+    // Change root(html) attribute value to current theme
     document
       .documentElement
       .attributes
